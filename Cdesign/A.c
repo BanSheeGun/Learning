41fd4410c569d030306943731ff18994dd17b9ed
Gunpowder <ccp750707@126.com>
2015-12-11 16:36:05 +0800

    Cdesgin V1

--- Cdesign/A.c
+++ Cdesign/A.c
@@ -9,25 +9,103 @@
 #include<string.h>
 #include<math.h>
 
+// define record
+struct Record
+{
+    struct Student *S;
+    struct Activity *A;
+    char PS[100];
+    struct Record *Next;
+};
+
+//define activity
+struct Activity
+{
+    int Number;
+    char Name[100];
+    char PS[100];
+    struct Record *Head;
+    struct Activity *Next;
+};
+
 //define student
 struct Student
 {
     char Name[10];
     int Number;
+    struct Record *Head;
     struct Student *Next;
 };
 
-// the size of Student;
-int size = sizeof(struct Student);
+typedef struct Record Rec;
+typedef struct Student Stu;
+typedef struct Activity Act;
+
+
+// the size of Structs;
+int StuSize = sizeof(struct Student);
+int ActSize = sizeof(struct Activity);
+int RecSize = sizeof(struct Record);
 // head will be meaningless and never changed and link the whole line;
-struct Student *head;
+Stu *StuHead;
+Rec *RecHead;
+Act *ActHead;
 int Order;
+
+int Iassign(char a[100]) {
+    freopen(a,"r",stdin);
+    return 0;
+}
+
+int Oassign(char a[100]) {
+    freopen(a,"w",stdout);
+    return 0;
+}
+
+int Faddrec() {
+    return 0;
+}
+
+int Faddstu() {
+    return 0;
+}
+
+int Faddact() {
+    return 0;
+}
+
+int addrec() {
+    return 0;
+}
+
+int addstu() {
+    return 0;
+}
+
+int addact() {
+    return 0;
+}
+
+int qstu() {
+    return 0;
+}
+
+int qact() {
+    return 0;
+}
+
+int qc() {
+    return 0;
+}
 
 int Init() {
     printf("Welcome to use this program\n");
-    head = (struct Student *) malloc (size);
-    head->Next = NULL;
-    head->Number = 0;
+    StuHead = (Stu *) malloc (StuSize);
+    StuHead->Next = NULL;
+    ActHead = (Act *) malloc (ActSize);
+    ActHead->Next = NULL;
+    RecHead = (Rec *) malloc (RecSize);
+    RecHead->Next = NULL;
     return 0;
 }
 
@@ -35,27 +113,38 @@ int ReadInit() {
     // some tips
     printf("If you want to exit,please input 0\n");
     printf("If you want to add a student,please input 1\n");
+    printf("If you want to add a activity,please input 2\n");
+    printf("If you want to find a student,please input 3\n");
+    printf("If you want to find a activity,please input 4\n");
+    printf("If you want to find all students,please input 5\n");
     printf("Please input the operate number and press the Enter\n");
     scanf("%d",&Order);
     return 0;
 }
 
-int add() {
+int EndFree() {
     return 0;
 }
 
 int main() {
     Init();
-    ReadInit();
+    Order = -1;
+
+    //work
     while (Order != 0) {
         ReadInit();
         switch (Order) {
-            case 1 : add();
-            default : break;
+            case 1 : addstu();
+            case 2 : addact();
+            case 3 : qstu();
+            case 4 : qact();
+            case 5 : qc();
+            case 0 : break;
+            default : printf("Noknown Order\n");
         }
     }
 
     // free the memory
-    free(head);
+    EndFree();
     return 0;
 }
