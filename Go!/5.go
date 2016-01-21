7f174ae901092e917f51223919c77aa4dacbcb41
Gunpowder <ccp750707@126.com>
2016-01-22 01:05:23 +0800

    cf

---
+++ Go!/5.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+    a := 2+3i;
+    b := 1+2i;
+    fmt.Printf("%v\n",a * b)
+}
---
+++ Go!/6.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+    monthdays := map[string]int {
+        "Jan": 31, "Feb": 28, "Mar": 31,
+        "Apr": 30, "May": 31, "Jun": 30,
+        "Jul": 31, "Aug": 31, "Sep": 30,
+        "Oct": 31, "Nov": 30, "Dec": 31,
+    }
+    for Mon, Day := range monthdays {
+        fmt.Printf("%s is %d\n",Mon, Day)
+    }
+}
